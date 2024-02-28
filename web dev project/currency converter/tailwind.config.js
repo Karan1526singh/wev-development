2fd20f016666b791906bcc1a408f480519e4ce01
Karan1526singh <108975738+Karan1526singh@users.noreply.github.com>
2024-02-28 23:17:35 +0530

    Add files via upload

---
+++ web dev project/currency converter/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      theme: {
+        fontFamily: {
+          'sans': ['Roboto', 'sans-serif'],
+          'serif': ['Georgia', 'serif'],
+          'mono': ['Menlo', 'monospace']
+        }
+    },
+  },
+  plugins: [],
+}
+}
+
